a8566718155a3306d2116540c7c3b864410d6539
Kevin Earls <kearls@redhat.com>
2020-12-01 12:45:54 +0100

    Checking OS and port order
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- main_test.go
+++ main_test.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"runtime"
 	"testing"
 )
 
-func TestOne(*testing.T) {
+func TestOne(t *testing.T) {
+	fmt.Printf("Runtime.GOOS is %v\n", runtime.GOOS)
+	for i:=0; i < 25 ; i++ {
+		ln, err := net.Listen("tcp", "localhost:0")
+		if err != nil {
+			t.Errorf("net.Listen got error %v\n", err)
+		} else {
+			fmt.Printf(">>>> Got address %s\n", ln.Addr().String())
+			ln.Close()
+		}
+	}
 	fmt.Println("It worked!!!")
 }
