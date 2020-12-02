19cd344f210ea0fdf03acb2ea42595228a820197
Kevin Earls <kearls@redhat.com>
2020-12-02 09:59:04 +0100

    WIP cleanup and minor changes
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- main.go
+++ main.go
@@ -2,21 +2,12 @@ package main
 
 import (
     "fmt"
-    "github.com/stretchr/testify/require"
-    "net"
     "runtime"
-    "strconv"
-    "testing"
 )
 
-type  portpair struct {
-    first string
-    last string
-}
-
 func main() {
-    exclusions := []portpair{{"49692","49791" }, {"49792", "49891"}, {"49892", "49991"}, {"49992", "50091"}, {"50092", "50191"}, {"50214", "50313"}, {"50498", "50597"}, {}}
-    fmt.Printf("%d\n", len(exclusions))
+    //exclusions := []portpair{{"49692","49791" }, {"49792", "49891"}, {"49892", "49991"}, {"49992", "50091"}, {"50092", "50191"}, {"50214", "50313"}, {"50498", "50597"}, {}}
+    //fmt.Printf("%d\n", len(exclusions))
     fmt.Printf("OS? %v \n", runtime.GOOS)
 
     /*excluded := 0
@@ -36,38 +27,3 @@ func main() {
     fmt.Printf("Excluded %d entries\n", excluded) */
 }
 
-// This should probably get the exclusions list itself if it's on windows?????
-func GetAvailablePort(t *testing.T, exclusions []portpair) uint16 {
-    portFound := false
-    var port string
-    var err error
-    for !portFound {
-        endpoint := GetAvailableLocalAddress(t)
-        _, port, err = net.SplitHostPort(endpoint)
-        require.NoError(t, err)
-        portFound = true
-        if runtime.GOOS == "windows" {   // FIXME FIXME change back to is windows!
-            for _, pair := range exclusions {
-                if port >= pair.first && port <= pair.last {
-                    fmt.Printf(">>>>>>>>> Excluded %s because of range %s to %s\n", port, pair.first, pair.last)  // TODO change to debug line
-                    portFound = false
-                    break
-                }
-            }
-        }
-    }
-
-    portInt, err := strconv.Atoi(port)
-    require.NoError(t, err)
-
-    return uint16(portInt)
-}
-
-func GetAvailableLocalAddress(t *testing.T) string {
-    ln, err := net.Listen("tcp", "localhost:0")
-    require.NoError(t, err)
-    // There is a possible race if something else takes this same port before
-    // the test uses it, however, that is unlikely in practice.
-    defer ln.Close()
-    return ln.Addr().String()
-}
--- main_test.go
+++ main_test.go
@@ -10,6 +10,7 @@ package main
 import (
 	"fmt"
 	"github.com/stretchr/testify/require"
+	"net"
 	"os/exec"
 	"runtime"
 	"strconv"
@@ -19,24 +20,35 @@ import (
 
 func TestExclusions(t *testing.T) {
 	if runtime.GOOS == "windows" {
-		exclusions := createExclustionsList(t)
-		fmt.Printf(">>>>>> Got %d exclusion pairs\n", len(exclusions))
-		for _, p := range exclusions {
-			fmt.Printf("\t%v\n", p)
-		}
+		//exclusions := createExclustionsList(t)
+		//fmt.Printf(">>>>>> Got %d exclusion pairs\n", len(exclusions))
+		//for _, p := range exclusions {
+		//	fmt.Printf("\t%v\n", p)
+		//}
 
-		port := GetAvailablePort(t, exclusions)
+		port := GetAvailablePort(t)
 		fmt.Printf("Got first port %d\n", port)
 
-		// HAK!  Add something we know will cause exclusions
-		newExclude := portpair{strconv.Itoa(int(port)), strconv.Itoa(int(port)+15)}
-		exclusions = append(exclusions, newExclude)
-		fmt.Printf("Added %v to exclusions\n", newExclude)
+		// HACK!  Add something we know will cause exclusions
+		//newExclude := portpair{strconv.Itoa(int(port)), strconv.Itoa(int(port)+15)}
+		//exclusions = append(exclusions, newExclude)
+		//fmt.Printf("Added %v to exclusions\n", newExclude)
 
-		secondPort := GetAvailablePort(t, exclusions)
+		secondPort := GetAvailablePort(t)
 		fmt.Printf("Got seondPort %d\n", secondPort)
 		require.Equal(t, port + 16, secondPort)
+	} else {
+		port := GetAvailablePort(t)
+		fmt.Printf("Got first port %d\n", port)
+		secondPort := GetAvailablePort(t)
+		fmt.Printf("Got seondPort %d\n", secondPort)
+		require.Equal(t, port + 1, secondPort)  // Is this always true?
 	}
+}
+
+type  portpair struct {
+	first string
+	last string
 }
 
 func TestHardcodedExclusions(t *testing.T) {
@@ -82,9 +94,25 @@ func createExclustionsList(t *testing.T) []portpair {
 	cmd := exec.Command("netsh", "interface",  "ipv4",  "show",  "excludedportrange", "protocol=tcp")
 	output, err := cmd.CombinedOutput()
 	require.NoError(t, err)
-	fmt.Printf("NETSH command got: \n%s\n", string(output))
+	fmt.Printf("NETSH command got: \n%s\n", string(output))   //FIXME remove
 
 	exclusions := getExclusionsList(string(output), t)
+
+	/// FIXME FIXME FIXME remove, hack for testing
+	// HACK!  Add something we know will cause exclusions
+	//
+	var port string
+	endpoint := GetAvailableLocalAddress(t)
+	_, port, err = net.SplitHostPort(endpoint)
+	require.NoError(t, err)
+
+	stupid, _ := strconv.Atoi(port)
+	p2:= strconv.Itoa(stupid + 10)
+	newExclude := portpair{port, p2}
+	exclusions = append(exclusions, newExclude)
+	fmt.Printf("Added %v to exclusions\n", newExclude)
+	// End of HACK
+
 	return exclusions
 }
 
@@ -94,7 +122,7 @@ func getExclusionsList(exclusionsText string, t *testing.T) []portpair {
 
 	parts := strings.Split(exclusionsText, "--------")
 	require.Equal(t, len(parts), 3)
-	portsText := strings.Split(parts[2], "*")  // TODO check for two
+	portsText := strings.Split(parts[2], "*")
 	require.Equal(t, len(portsText), 2)
 	lines := strings.Split(portsText[0], "\n")
 	for _, line := range lines {
@@ -106,5 +134,46 @@ func getExclusionsList(exclusionsText string, t *testing.T) []portpair {
 		}
 	}
 	return exclusions
+}
+
+
+func GetAvailablePort(t *testing.T) uint16 {
+	var exclusions [] portpair
+	if runtime.GOOS == "windows" {
+		exclusions = createExclustionsList(t)
+	}
+
+	portFound := false
+	var port string
+	var err error
+	for !portFound {
+		endpoint := GetAvailableLocalAddress(t)
+		_, port, err = net.SplitHostPort(endpoint)
+		require.NoError(t, err)
+		portFound = true
+		if runtime.GOOS == "windows" {
+			for _, pair := range exclusions {
+				if port >= pair.first && port <= pair.last {
+					fmt.Printf(">>>>>>>>> Excluded %s because of range %s to %s\n", port, pair.first, pair.last)  // TODO change to debug line or remove
+					portFound = false
+					break
+				}
+			}
+		}
+	}
+
+	portInt, err := strconv.Atoi(port)
+	require.NoError(t, err)
+
+	return uint16(portInt)
+}
+
+func GetAvailableLocalAddress(t *testing.T) string {
+	ln, err := net.Listen("tcp", "localhost:0")
+	require.NoError(t, err)
+	// There is a possible race if something else takes this same port before
+	// the test uses it, however, that is unlikely in practice.
+	defer ln.Close()
+	return ln.Addr().String()
 }
 
